f58f62212c67068224dd0c699d12138307a4cb0c
Pasha <p.karvatskyi@avivi.com.ua>
2023-07-30 23:40:34 +0300

    Added new test file

---
+++ src/api_test.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from vector_store import get_answer_from_vector_store
+
+app = FastAPI()
+
+
+@app.get("/api/send/")
+def example(request: Request) -> dict:
+    return {"data": "okey"}
